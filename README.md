6e8726d783bd48a88c27857a244cda16e91d802c
sammyrhymes <samuelmwendwa5996@gmail.com>
2023-06-06 21:27:58 +0300

    merge solving

--- README.md
+++
@@ -1,1 +0,0 @@
-this is a simple to-do app
--- app.py
+++ app.py
@@ -31,47 +31,6 @@ def index():
     # render template with retreived data
     return render_template('index.html', todos=todos)
 
-# ADDing new tasks
-@app.route('/add', methods=['POST'])
-def add_task():
-
-    # store value gotten from form in a varibale
-    task = request.form.get('task')
-
-    # establish a conncetion to the database
-    con = sqlite3.connect('todo.db')
-
-    # create a cursor
-    cur = con.cursor()
-
-    # execute insert query
-    cur.execute('INSERT into TODOS (task, completed) VAlUES (?,?)', (task, 0))
-
-    # commit the changes
-    con.commit()
-
-    # close conncetion
-    con.close()
-
-    #redirect
-    return redirect(url_for('index'))
-
-# UPDATE existing tasks
-@app.route('/update/<int:todo_id>', methods=['POST'])
-def update_task(todo_id):
-    conn = sqlite3.connect('todo.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT completed FROM todos WHERE id = ?', (todo_id,))
-    result = cursor.fetchone()
-    if result:
-        completed = 0 if result[0] else 1
-        cursor.execute('UPDATE todos SET completed = ? WHERE id = ?', (completed, todo_id))
-        conn.commit()
-    conn.close()
-    return redirect(url_for('index'))
-
-
-
 # only start development server if runnnig this file directly
 if __name__ == '__main__':
     app.run(debug=True)
